57b2e02bf550aca180e549d058241a8b07d8a578
Mike J <mjanes@fischerhomes.com>
2022-05-04 19:34:15 -0400

    popup modal working

---
+++ src/components/AddItemModal.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AddItemModal({
+  open,
+  handleClose,
+}) {
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"SHOPPING LIST"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose} autoFocus variant="contained">
+            Add Item
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
