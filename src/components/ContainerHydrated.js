7174c7d53bf418a4f5ea688348353f4afc5690a4
Mike J <mjanes@fischerhomes.com>
2022-05-04 22:29:31 -0400

    add item from within hydrated container working

--- src/components/ContainerHydrated.js
+++ src/components/ContainerHydrated.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 // MUI
 import { Box } from '@mui/system';
+import Button from '@mui/material/Button';
 // custom
 import Item from './Item'
-
+import AddItemModal from './AddItemModal';
 
 export default function ContainerHydrated({
   items,
 }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const itemsList = items.map((item) => {
     return <Item key={item.id} item={item} />
   })
   
   return (
     <Box>
+      <Button
+        variant="contained"
+        onClick={handleOpen}
+      >
+        Add Item
+      </Button>
       {itemsList}
+
+      <AddItemModal
+        open={open}
+        handleClose={handleClose}
+      />
     </Box>
   )
 }
